e7b49523f8b421bdb70e26377ad1ec57bb397e9e
hanielf <hanielxx@outlook.com>
2018-05-27 22:15:59 +0000

    update ui

--- src/ui/LoginPage.java
+++ src/ui/LoginPage.java
@@ -1,81 +1,120 @@
 package ui;
 
+import ui.MainClass;
 import javax.swing.*;
 import javax.swing.event.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.awt.event.MouseAdapter;
 
-public class LoginPage implements ActionListener{
-	private JFrame jf=new JFrame("Login Page");
+public class LoginPage {
+	public static JFrame jf=new JFrame("Login Page");
 	private JPanel jp_main;
-	private JPanel jp_center=new JPanel();
-	private JPanel jp_top=new JPanel();
-	private JPanel jp_bow=new JPanel();//用这两个调整主面板界面
 	
-	private JLabel jl_usr=new JLabel("用户名");
-	private JLabel jl_pwd=new JLabel("密码");
+	private JLabel jl_usr=new JLabel("Account");
+	private JLabel jl_pwd=new JLabel("Password");
+	private JLabel jl_title=new JLabel("Login System");
+	
 	private JTextField jtf_usr=new JTextField();
-	private JPasswordField jpf=new JPasswordField();
+	private JPasswordField jpf=new JPasswordField();	
 	
-	private JButton jb_login=new JButton("登陆");
-	private JButton jb_register=new JButton("注册");
+	private JButton jb_login=new JButton("Sign in");
+	private JButton jb_register=new JButton("Sign Up");
+	private JButton jb_forget=new JButton("Forget Password? ");	
 	
-	private int width=600;
-	private int height=400;
+	private String id;
+	private String pwd;
+	private int width=400;
+	private int height=270;
 	
 	@SuppressWarnings("serial")
 	public LoginPage() {
-		jp_main=new JPanel(new BorderLayout()){  
+        drawMain();
+        initMain();
+        initFrame();
+     
+        jb_login.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+                id=jtf_usr.getText();
+                pwd=new String(jpf.getPassword());
+                if(MainClass.db.loginJudge(id, pwd)) {
+                	MainClass.mp.jf.setVisible(true);
+                	jf.setVisible(false);
+                }
+        	}
+        });
+        
+
+
+	}
+	
+	public void drawMain() {
+		jp_main=new JPanel(){  
             @Override  
             public void paintComponent(Graphics g) {  
                 Graphics2D g2=(Graphics2D)g;   
                 super.paintComponents(g);  
-                Image image=Toolkit.getDefaultToolkit().getImage("F:\\Eclipse\\JAVA\\BookManager\\source\\PureColor.jpg");  
+                Image image = new ImageIcon("F:\\Eclipse\\JAVA\\BookManager\\source\\PureColor.jpg").getImage(); 
+                //Image image=Toolkit.getDefaultToolkit().getImage("F:\\Eclipse\\JAVA\\BookManager\\source\\PureColor.jpg");  
                 g2.drawImage(image,0,0,this.getWidth(),this.getHeight(),this);  
             }  
          }; 
-		jp_main=new JPanel(new BorderLayout());
-        initCenter();
-        
-        jp_top.setPreferredSize(new Dimension(jf.getWidth(),30));
-        jp_bow.setPreferredSize(new Dimension(jf.getWidth(),20));
-        jp_top.setOpaque(false);
-        jp_bow.setOpaque(false);
-      
-        jp_main.add(jp_top,BorderLayout.NORTH);
-        jp_main.add(jp_bow,BorderLayout.SOUTH);
-	 	jp_main.add(jp_center,BorderLayout.CENTER);
-        
+	}
+	
+	public void initFrame() {
+		jf.setSize(width,height);
         //获得屏幕高度宽度，用来设置居中
         Dimension scr=Toolkit.getDefaultToolkit().getScreenSize();  
-        jf.setLocation((scr.width-jf.getWidth())/2-width/2,(scr.height-jf.getHeight())/2-height/2); 
-        
-	 	jf.setSize(width,height);
+        jf.setLocation((scr.width-jf.getWidth())/2,(scr.height-jf.getHeight())/2);   
+
         jf.setContentPane(jp_main);
 	 	jf.setVisible(true);
 	 	jf.setResizable(false);
 	 	jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
-	public void initCenter() {
-		jp_center.setOpaque(false);
-        //jl_usr.setBounds(,30,60,20);  
-        jl_pwd.setBounds(80,55,60,20); 
-        jp_center.add(jl_usr);
-        jp_center.add(jl_pwd);
-	}
-	
-	public void setJPanel() {
+	public void initMain() {
+		jp_main.setOpaque(false);
+        jp_main.setLayout(null);
 		
-	}
-	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		//x,y,width,height
+		jl_title.setBounds(145,10,150,30);
+		jl_title.setFont(new Font("Arial",Font.BOLD,19));
 		
+		jl_usr.setBounds(60, 60, 100, 30);
+		jl_usr.setFont(new Font("consolas",Font.PLAIN,16));
+        jtf_usr.setBounds(140,60,190,30);     
+        
+        jl_pwd.setBounds(60,100,100,30);
+        jl_pwd.setFont(new Font("consolas",Font.PLAIN,16));
+        jpf.setBounds(140,100,190,30);
+  
+        jb_register.setBounds(80, 150, 90, 30);
+        jb_register.setFont(new Font("Lucida Family",Font.PLAIN,15));
+        jb_login.setBounds(220, 150, 90, 30);
+        jb_login.setFont(new Font("Lucida Family",Font.PLAIN,15));
+        
+        jb_forget.setBounds(115,190,160,30);
+        jb_forget.setBackground(new Color(0));
+        jb_forget.setBorderPainted(false);
+        jb_forget.setOpaque(false); 
+        jb_forget.setFont(new Font("Lucida Family",Font.PLAIN,13));
+
+        jp_main.add(jl_title);
+        jp_main.add(jl_usr);
+        jp_main.add(jtf_usr);
+        jp_main.add(jl_pwd);
+        jp_main.add(jpf);
+        jp_main.add(jb_login);
+        jp_main.add(jb_register);
+        jp_main.add(jb_forget);
+        
+        jp_main.setSize(width,height);
+        jp_main.setVisible(true);
 	}
-	
-	
-	
+
+
 }
--- src/ui/ShowUsers.java
+++ src/ui/ShowUsers.java
@@ -1,40 +1,41 @@
 package ui;
+
 import javax.swing.*;
 import java.awt.*;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.*;
 
-public class ShowBooks {
+public class ShowUsers {
 	private JTable jt;
 	public JScrollPane jsp;
 	private Vector rowData,columnNames;	
 	private ResultSet rs=null;
 	
-	public ShowBooks() {
+	public ShowUsers() {
 		rowData = new Vector();
 		columnNames = new Vector();
 		ArrayList arr = new ArrayList();
 
-		columnNames.add("Title");
-		columnNames.add("Author");
-		columnNames.add("Price");
+		columnNames.add("Name");
 		columnNames.add("ID");
-		columnNames.add("Number");
+		columnNames.add("IsManager");
 		
-		//迭代获取map中值
-		arr=MainClass.db.dbGet("select * from books");
+		//从键值获取map中值
+		arr=MainClass.db.dbGet("select uname,uid,upwd,ismanager from users");
 		try {
 			Iterator ait = arr.iterator();
 			while(ait.hasNext()) {
 				Map map = (Map)ait.next();
-				Iterator iter = map.entrySet().iterator();
 				Vector tmp = new Vector();
-				while(iter.hasNext()) {
-					Map.Entry entry = (Map.Entry) iter.next();
-					Object val = entry.getValue();
-					tmp.add(val.toString());
-				}
+
+				tmp.add(map.get("uname"));
+				tmp.add(map.get("uid"));
+				if((int)map.get("ismanager")==1)
+					tmp.add("Yes");
+				else
+					tmp.add("No");
+				
 				rowData.add(tmp);
 			}
 		} catch (Exception e) {
@@ -48,5 +49,4 @@ public class ShowBooks {
 		jsp.setVisible(true);
 		//jsp.setPreferredSize(new Dimension(MainClass.mp.getWidth()-130,MainClass.mp.getHeight()));
 	}
-
 }
