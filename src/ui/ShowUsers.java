5b08314536e5ec21a76d6364ed8bd3fa1cdc18f5
HanielF <Hanielxx@outlook.com>
2018-06-04 19:00:15 +0000

    add function of search users

--- src/ui/ShowUsers.java
+++ src/ui/ShowUsers.java
@@ -1,33 +1,96 @@
 package ui;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.*;
 
 public class ShowUsers {
+	private JLabel jl_hint;
+	private JComboBox<String> jcb;
+	private JTextField jtf;
+	private JButton jb_search;
+	
 	private JTable jt;
 	public JScrollPane jsp;
+	public JPanel jp;//最外层
+	private JPanel jp_top;
 	
 	private int width = MainPage.getWidth()-MainClass.mp.sbar.getWidth();
 	private int height = MainPage.getHeight()-Banner.getHeight();
+	private int top_height = 60;
 	
 	private Vector rowData,columnNames;	
-	private ResultSet rs=null;
 	
 	public ShowUsers() {
+		initVec();
+		initTable();
+		jsp = new JScrollPane(jt);
+		jsp.setPreferredSize(new Dimension(width,height-top_height));
+		jsp.setVisible(true);
+        jsp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        jsp.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+
+		initTop();
+		jp_top = new JPanel();
+		jp_top.setLayout(null);
+		jp_top.setPreferredSize(new Dimension(width,top_height));
+		jp_top.setVisible(true);
+		jp_top.add(jl_hint);
+		jp_top.add(jcb);
+		jp_top.add(jtf);
+		jp_top.add(jb_search);
+	
+		jp = new JPanel(new BorderLayout());
+		jp.setPreferredSize(new Dimension(width,height));
+		jp.setVisible(true);
+		jp.add(jp_top, BorderLayout.NORTH);
+		jp.add(jsp,BorderLayout.CENTER);
+		
+		addListener();
+	}
+	
+	public void initVec() {
 		rowData = new Vector();
 		columnNames = new Vector();
-		ArrayList arr = new ArrayList();
-
 		columnNames.add("Name");
 		columnNames.add("ID");
 		columnNames.add("IsManager");
-		
+	}
+	
+	//调用setTable()
+	public void initTable() {
+		//init jt and jsp
+		setTable("select uname,uid,ismanager from users;");
+		jt = new JTable(rowData,columnNames);
+		jt.setFont(new Font("consolas",Font.PLAIN,18));
+		jt.getTableHeader().setPreferredSize(new Dimension(width,27));
+		jt.getTableHeader().setFont(new Font("consolas",Font.PLAIN,19));
+		jt.setRowHeight(27);
+		jt.setLocation(0,0);
+		jt.setVisible(true);
+		setTableColumnCenter(jt);
+	}
+	
+	public void setTableColumnCenter(JTable table){  
+	    DefaultTableCellRenderer r = new DefaultTableCellRenderer();     
+	    r.setHorizontalAlignment(JLabel.CENTER);     
+	    table.setDefaultRenderer(Object.class, r);  
+	}
+	
+	public void setTable(String query) {
+		if(!rowData.isEmpty()) rowData.clear();
+		ArrayList arr = new ArrayList();
+
 		//从键值获取map中值
-		arr=MainClass.db.dbGet("select uname,uid,upwd,ismanager from users");
+		arr=MainClass.db.dbGet("select uname,uid,ismanager from users");
 		try {
 			Iterator ait = arr.iterator();
 			while(ait.hasNext()) {
@@ -46,15 +109,70 @@ public class ShowUsers {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public void initTop() {
+		jl_hint = new JLabel("查询方式");
+		jl_hint.setSize(80,30);
+		jl_hint.setLocation(80,top_height/2-jl_hint.getHeight()/2);
+		jl_hint.setFont(new Font("consolas",Font.PLAIN,19));
+		jl_hint.setVisible(true);
 		
-		//init jt and jsp
-		jt = new JTable(rowData,columnNames);
-		jt.setFont(new Font("consolas",Font.PLAIN,18));
-		jt.getTableHeader().setFont(new Font("consolas",Font.PLAIN,19));
-		jt.setRowHeight(25);
+		String[] choice = {"姓名","ID","管理员"};
+		jcb = new JComboBox<String>(choice);
+		jcb.setSelectedIndex(0);
+		jcb.setEditable(false);
+		jcb.setSize(100,30);
+		jcb.setFont(new Font("consolas",Font.PLAIN,19));
+		jcb.setLocation(180,top_height/2-jcb.getHeight()/2);
+		jcb.setVisible(true);
 		
-		jsp = new JScrollPane(jt);		
-		jsp.setPreferredSize(new Dimension(width,height));
-		jsp.setVisible(true);
+		jtf = new JTextField();
+		jtf.setSize(140,30);
+		jtf.setLocation(300,top_height/2-jtf.getHeight()/2);
+		jtf.setFont(new Font("consolas",Font.PLAIN,19));
+		jtf.setVisible(true);
+		
+		jb_search = new JButton("查询用户");
+		jb_search.setSize(120,30);
+		jb_search.setLocation(460,top_height/2-jb_search.getHeight()/2);
+		jb_search.setFont(new Font("consolas",Font.PLAIN,19));
+		jb_search.setVisible(true);
+	}
+	
+	//设置表格内容居中
+	public void updatePanel(String query) {
+		setTable(query);
+		jt.validate();
+		jt.updateUI();
+		jsp.validate();
+		jsp.updateUI();
+	}
+	
+	public void addListener() {
+		jb_search.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				String mode = (String)jcb.getSelectedItem();
+				String query = null;
+				switch(mode) {
+				case "全部":
+					query = "select uname,uid,ismanager form users;";
+					break;
+				case "姓名":
+					query = "select uname,uid,ismanager from users where uname='"+jtf.getText()+"';";
+					System.out.println(query);
+					break;
+				case "ID" :
+					query = "select uname,uid,ismanager from users where uid='"+jtf.getText()+"';";
+					break;
+				case "管理员":
+					query = "select uname,uid,ismanager from users where ismanager='1';";
+					break;
+				default:
+					query = "select uname,uid,ismanager form users;";
+				}
+				updatePanel(query);
+			}
+		});
 	}
 }
